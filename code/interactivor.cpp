4e2999bedb067e28cfcfb9e965dbbd0840c0eb4b
Chuzyh <Chuzyh>
2022-09-18 02:28:14 +0800

    task3_code

--- code/interactivor.cpp
+++ code/interactivor.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 #include "geometry.h"
 
-point p[N],s[N],r[N],la[N];
-const double sample[][2]={{0,0},{100,10},{98,40.10},{112,80.21},{105,119.75},{98,159.86},{112,199.96},{105,240.07},{98,280.17},{112,320.28}};
-
+point p[N],s[N],la[N];
+double th[N],ri[N];
+const double sample[][2]={{0,0},{100,0},{98,40.10},{112,80.21},{105,119.75},{98,159.86},{112,199.96},{105,240.07},{98,280.17},{112,320.28}};
 double ang[N];
 void move(int p,point del)
 {
@@ -19,28 +19,9 @@ int main()
     {
         s[i]=get_xy(radius,(double)(i-1)/n*2*pi);
         p[i]=get_xy(sample[i][0],sample[i][1]/360*2*pi);
-        r[i]=s[i];r[i].x+=rand_double()*5.0;r[i].y+=rand_double()*5.0;
-        //cout<<s[i].x<<' '<<s[i].y<<' '<<p[i].x<<' '<<p[i].y<<' '<<r[i].x<<' '<<r[i].y<<' ';
-        cout<<len(r[i])<<' '<<atan2(r[i].y,r[i].x)/(2*pi)*360<<endl;
-        
+        la[i]=(point){0,0};
     }
-    
-    /*for(int i=2;i<=9;i++)
-    {
-        int tot=0;
-        double L=(double)40*(i-1)/360*2*pi,R=L;
-        L-=2.0*pi*10/360,R+=2.0*pi*10/360;
-        double ang=pi-angle(p[1],p[0],p[i])-angle(p[i],p[0],p[1]);
-        while(R-L>eps)
-        {
-            double mid=(L+R)/2;tot++;
-            point delta=(point){2*cos(mid),2*sin(mid)};delta=delta+p[i];
-            double tmp_ang=pi-angle(p[1],p[0],delta)-angle(delta,p[0],p[1]);
-            if(i<=5)if(tmp_ang>ang)R=mid;else L=mid;
-            else if(tmp_ang<ang)R=mid;else L=mid;
-        }
-        printf("%.8lf %.8lf %d\n",(L+R)/2/pi*180,(L+R)/2/pi*180-sample[i][1],tot);
-    }*/
+        
     ask(0,1);
     ask(0,2,3,4);
     ask(0,5,6,7);
@@ -49,7 +30,7 @@ int main()
     for(int i=2;i<=9;i++)
     {
         L[i]=(double)40*(i-1)/360*2*pi,R[i]=L[i];
-        L[i]-=2.0*pi*10/360,R[i]+=2.0*pi*10/360;
+        L[i]-=2.0*pi*20/360,R[i]+=2.0*pi*20/360;
         ang[i]=pi-angle(p[1],p[0],p[i])-angle(p[i],p[0],p[1]);
     }
     for(int T=1;T<=20;T++)
@@ -58,7 +39,7 @@ int main()
         if(R[i]-L[i]>eps)
         {
             double mid=(L[i]+R[i])/2;
-            point delta=(point){2*cos(mid),2*sin(mid)};
+            point delta=(point){2*cos(mid),2*sin(mid)};delta=delta*0.5*(1);
             move(i,delta);
             delta=delta+p[i];
             double tmp_ang=pi-angle(p[1],p[0],delta)-angle(delta,p[0],p[1]);
@@ -71,7 +52,21 @@ int main()
         ask(0,8,9);
     }
     L[1]=R[1]=(L[2]+R[2])/2-ang[2];
-    for(int i=1;i<=9;i++)printf("%.8lf ",(L[i]+R[i])/2/2/pi*360);
+
+    double sum_err=0;
+    for(int i=1;i<=9;i++)
+    {
+        th[i]=(L[i]+R[i])/2;
+        int ne=i+1;if(ne==10)ne=1;
+        double db=i!=1,da=ne!=1;
+        double alpha1=angle(p[i],p[0],p[ne]),alpha2=angle(p[i]+la[i],p[0],p[ne]+la[ne]);
+        double beta1=angle(p[ne],p[0],p[i]),beta2=angle(p[ne]+la[ne],p[0],p[i]+la[i]);
+        double b=(sin(alpha2)/sin(beta2)*db-da)/(sin(alpha1)/sin(beta1)-sin(alpha2)/sin(beta2));
+        ri[i]=b;
+    }
+    for(int i=1;i<=9;i++)move(i,get_xy(ri[i],th[i])-s[i]);
+    for(int i=1;i<=9;i++)s[i].out(),(p[i]-la[i]).out();
+
     return 0;
 }
 
